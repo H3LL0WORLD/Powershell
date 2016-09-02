a4c1d7ce8c5e8f1a1ff2c34dc1b0d08d49c8b69a
H3LL0WORLD <holamundoyt@gmail.com>
2016-09-01 23:41:20 -0500

    Create Paste.ee.ps1
    Paste.ee api wrap up for powershell

---
+++ Paste.ee.ps1
@@ -0,0 +1,43 @@
+function _Invoke-Request
+{
+	param (
+		$url,
+		$parametros
+	)
+	$bytes = [System.Text.Encoding]::ASCII.GetBytes($parametros)
+	$ch = [System.Net.WebRequest]::Create($url)
+	$ch.Method = "POST";
+	$ch.ContentType = "application/x-www-form-urlencoded"
+	$ch.ContentLength = $bytes.Length
+
+	$stream = $ch.GetRequestStream()
+	$stream.Write($bytes, 0, $bytes.Length)
+	$stream.Flush()
+	$stream.Close()
+	$resp = $ch.GetResponse()
+	$sr = [System.IO.StreamReader] $resp.GetResponseStream()
+	$return = $sr.ReadToEnd().Trim()
+	return $return
+}
+
+function New-Paste
+{	
+	param (
+		$api_key = "",
+		$format = "simple",
+		$language = "plain",
+		$description = '',
+		$paste = ''
+	)
+	$formats = "json", "xml", "simple"
+	
+	$url = "http://paste.ee/api"
+
+	$description			= [uri]::EscapeDataString($description);
+	$paste			= [uri]::EscapeDataString($paste);
+
+	$params = "key=$api_key&format=$format&language$language&description=$description&paste=$paste&format=json"
+	write-host $params
+
+	_Invoke-Request $url $params
+}
