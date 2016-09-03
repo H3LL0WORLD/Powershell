7f75ecccb34bb6e70f197fa79c6ee9f4790f18a2
H3LL0WORLD <holamundoyt@gmail.com>
2016-09-03 07:10:21 -0500

    Create Clean-RunMRU.psm1

---
+++ Utilities/Clean-RunMRU.psm1
@@ -0,0 +1,44 @@
+function Clean-RunMRU
+{
+	param(
+		[Switch]$All,
+		[Switch]$Last,
+		[String]$Pattern
+	)
+	
+	$RunMRUPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU\'
+	if ($All) {
+		if ($Last -or $Pattern) {
+			Write-Warning 'Solo seleccionar una opcion.'
+			break
+		}
+		Remove-ItemProperty -Path $RunMRUPath -Name * -Force
+	}
+	if ($Last) {
+		if ($All -or $Pattern) {
+			Write-Warning 'Solo seleccionar una opcion.'
+			break
+		}
+		$OldMRUList = (Get-ItemProperty -Path $RunMRUPath).MRUList
+		$LastCommandID = $OldMRUList[0]
+		Remove-ItemProperty -Path $RunMRUPath -Name $LastCommandID -Force
+		$NewMRUList = $OldMRUList.Remove(0,1)
+		Set-ItemProperty -Path $RunMRUPath -Name 'MRUList' -Value $NewMRUList -Force
+	}
+	
+	if ($Pattern) {
+		if ($All -or $Last) {
+			Write-Warning 'Solo seleccionar una opcion.'
+			break
+		}
+		$RunMRU = Get-ItemProperty -Path $RunMRUPath
+		$MRUList = $RunMRU.MRUList
+		0 .. ($MRUList.Length - 1) | foreach { $MRUList[$_] } | foreach {
+			if ($RunMRU.$_.Contains($Pattern)) {
+				Remove-ItemProperty -Path $RunMRUPath -Name $_ -Force
+				$NewMRUList = $MRUList.Replace("$_","")
+				Set-ItemProperty -Path $RunMRUPath -Name 'MRUList' -Value $NewMRUList -Force
+			}
+		}
+	}
+}
